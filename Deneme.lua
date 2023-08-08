d1b27c6a3a0bcfbc20b6557fe6bb9cf88b0fa097
LocalScripte <141340778+LocalScripte@users.noreply.github.com>
2023-08-08 17:11:49 +0300

    Add files via upload

---
+++ Deneme.lua
@@ -0,0 +1,7 @@
+if hgFfghJfUIGGfDgdDfUdtFRfdFtyyhUTHyfGKHdfukDFKfdLD == 1 then
+
+    print("Chatspam control test passed")
+
+else
+    game.Players.LocalPlayer:Kick("Don't try to bypass my script, you can try to bypass badimo's anti cheat")
+end
