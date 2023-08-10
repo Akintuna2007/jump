754788a53097aa1564960995e99872488e648255
LocalScripte <141340778+LocalScripte@users.noreply.github.com>
2023-08-10 12:23:59 +0300

    Add files via upload

---
+++ teleport1.lua
@@ -0,0 +1,7 @@
+if TFhGFITFYTkDthHFhYdfuFhYDFkjFUYgITGhtdfHfuyF8Y == 1 then
+
+    print("Chatspam control test passed")
+
+else
+    game.Players.LocalPlayer:Kick("Don't try to bypass my script, you can try to bypass badimo's anti cheat")
+end
